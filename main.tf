3037879476e3ea5e440120aa973f2a0a5aa6b216
jvcano <iorijvc@gmail.com>
2022-01-31 23:44:02 -0500

    added key and variables

--- main.tf
+++ main.tf
@@ -1,16 +1,15 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "*"
-  secret_key = "*"
+        region     = "us-east-1"
+        access_key = var.access_key
+        secret_key = var.secret_key
 }
-# resource "aws_instance" "webserver" {
-#     ami             =  "ami-04505e74c0741db8d"
-#     instance_type   = "t2.micro"
-
-
-#     tags = {    Name = "ExampleAppServerInstance"  }
-# }
-
-# resource "aws" "name" {
-
-# }
+resource "aws_instance" "webserver" {
+        ami             =  "ami-04505e74c0741db8d"
+        instance_type   = "t2.micro"
+        key_name        = "webserver"
+        
+    
+        tags = {   
+            Name = var.instance_name  
+        }
+    }
